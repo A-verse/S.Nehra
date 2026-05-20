37bf72b93d9d01d4bf8b82f13025fd74e7b3f0bc
Mayank Kaushik <mayankkaushik7264@gmail.com>
2026-05-20 10:17:31 +0530

    added frontend cors url

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
 
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://s-nehra.onrender.com",
         changeOrigin: true,
         secure: false,
       },
